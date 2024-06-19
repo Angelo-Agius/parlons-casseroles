d101367f9e683284d48f46e9b1267aba4bc33348
AngeloAgius-dev <agiusangelo13@gmail.com>
2024-06-19 22:22:02 +0200

    site terminée

--- src/script/headerFooterManager.js
+++ src/script/headerFooterManager.js
@@ -5,8 +5,8 @@ class CustomHeader extends HTMLElement {
             <ul>
                 <li><a href="./home.html">Accueil</a></li>
                 <li><a href="#history">Histoire de la casserole</a></li>
-                <li><a href="">2</a></li>
-                <li><a href="">3</a></li>
+                <li><a href="./home.html#metherials">Les matheriaux des casseroles</a></li>
+                <li><a href="./home.html#size">Les tailles des casseroles</a></li>
             </ul>
         </div>
         `
@@ -16,9 +16,10 @@ class CustomHeader extends HTMLElement {
 class CustomFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <p>
-            © 2024 Copyright - Agius Angelo
-        </p>
+        <h6>
+            © 2024 Copyright - Agius Angelo <br>
+            Powered by <a href="https://github.com" target="_blank">GitHub</a>
+        </h6>
         `
     }
 }
